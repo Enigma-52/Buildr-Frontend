91798e7bed9b8749c57ac1193f5e5b006d1c3db6
Rohit Singh <rohit.singh.33521@gmail.com>
2024-06-11 20:57:13 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import Form from './pages/Form';
 import Payment from './pages/Payment';
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  in: {
+    opacity: 1,
+    scale: 1,
+  },
+  out: {
+    opacity: 0,
+    scale: 0.8,
+  },
+};
+
+const pageTransition = {
+  type: 'spring',
+  stiffness: 70,
+  damping: 20,
+};
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/signin" element={<PageWrapper><SignIn /></PageWrapper>} />
+        <Route path="/dashboard" element={<PageWrapper><Dashboard /></PageWrapper>} />
+        <Route path="/form" element={<PageWrapper><Form /></PageWrapper>} />
+        <Route path="/payment" element={<PageWrapper><Payment /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="/payment" element={<Payment />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </Router>
   );
